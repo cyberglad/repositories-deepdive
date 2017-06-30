a83f28c59aefa950c2db493a2797d9c6b439ec10
cyberglad <cyberglad@mail.ru>
2017-06-30 10:57:43 +0200

    updated version

--- src/main/java/de/olivergierke/deepdive/ApplicationConfig.java
+++ src/main/java/de/olivergierke/deepdive/ApplicationConfig.java
@@ -16,8 +16,20 @@
 package de.olivergierke.deepdive;
 
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.Database;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+import org.springframework.transaction.PlatformTransactionManager;
+
+import javax.sql.DataSource;
+import java.util.Properties;
 
 /**
  * Application configuration.
@@ -30,4 +42,29 @@ import org.springframework.context.annotation.Configuration;
 @ComponentScan
 class ApplicationConfig {
 
+    @Bean
+    public DataSource conferenceDataSource () {
+        EmbeddedDatabaseBuilder builder = new EmbeddedDatabaseBuilder();
+        return builder.setType(EmbeddedDatabaseType.HSQL).build();
+    }
+    @Bean
+    public LocalContainerEntityManagerFactoryBean entityManagerFactory(){
+        HibernateJpaVendorAdapter va = new HibernateJpaVendorAdapter();
+        va.setDatabase(Database.HSQL);
+        va.setGenerateDdl(true);
+        LocalContainerEntityManagerFactoryBean emFactory = new LocalContainerEntityManagerFactoryBean();
+        emFactory.setJpaVendorAdapter(va);
+        Properties jpaProperties = new Properties();
+        jpaProperties.setProperty("hibernate.hbm2ddl.auto", "create");
+        emFactory.setJpaProperties(jpaProperties);
+        emFactory.setPackagesToScan(getClass().getPackage().getName());
+        emFactory.setDataSource(conferenceDataSource());
+        return emFactory;
+    }
+    @Bean
+    public PlatformTransactionManager transactionManager() {
+        JpaTransactionManager transactionManager = new JpaTransactionManager();
+        transactionManager.setEntityManagerFactory(entityManagerFactory().getObject());
+        return transactionManager;
+    }
 }
